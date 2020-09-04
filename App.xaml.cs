e3e0dce3d21f3cf69e7c694082b985ac44a295a6
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2020-09-04 17:42:54 -0500

    Commit 2

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Parcial_SebastianGomez
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
